64f666376130d5cfa5f8ffa7ba6c95c8fb93255f
Ali <62478970+aliinium@users.noreply.github.com>
2024-07-13 00:52:00 +0330

    fix searchpage some bugs

--- src/main/resources/static/js/searchpagecontroller.js
+++ src/main/resources/static/js/searchpagecontroller.js
@@ -1,14 +1,9 @@
-// Create a new AbortController instance
-const controller = new AbortController();
-const signal = controller.signal;
-
 // Make the fetch request with the signal
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const search = urlParams.get('s');
 encodeURIComponent(search).replace(/%20/g, "+");
 const urldata = "https://omdbapi.com/?apikey=1a5410af&s=" + search;
-const fetchPromise = fetch(urldata, { signal });
 
 fetch(urldata)
     .then(response => response.json())
@@ -18,30 +13,46 @@ fetch(urldata)
             const container = document.querySelector('.row.px-4.px-lg-5.searchItemBody');
 
             results.forEach(result => {
-                const itemContent = document.createElement('div');
-                itemContent.className = 'col searchItemContent';
+                if (result.Poster !== "N/A") {
+                    const itemContent = document.createElement('div');
+                    itemContent.className = 'col searchItemContent';
 
-                const link = document.createElement('a');
-                link.id = 'searchItemLink';
-                link.href = `movie?id=${result.imdbID}`;
+                    const link = document.createElement('a');
+                    link.id = 'searchItemLink';
+                    link.href = `movie?id=${result.imdbID}`;
 
-                const poster = document.createElement('img');
-                poster.className = 'searchItemPoster';
-                poster.id = 'searchItemPoster';
-                poster.src = result.Poster;
+                    const poster = document.createElement('img');
+                    poster.className = 'searchItemPoster';
+                    poster.id = 'searchItemPoster';
+                    poster.src = result.Poster;
 
-                const name = document.createElement('div');
-                name.className = 'searchItemName';
-                name.id = 'searchItemName';
-                name.textContent = result.Title;
+                    const name = document.createElement('div');
+                    name.className = 'searchItemName';
+                    name.id = 'searchItemName';
+                    name.textContent = result.Title + " (" + result.Year + ")";
 
-                link.appendChild(poster);
-                link.appendChild(name);
-                itemContent.appendChild(link);
-                container.appendChild(itemContent);
+                    link.appendChild(poster);
+                    link.appendChild(name);
+                    itemContent.appendChild(link);
+                    container.appendChild(itemContent);
+                }
             });
         } else {
-            console.error('No results found');
+            const container = document.querySelector('.row.px-4.px-lg-5.searchItemBody');
+            const itemContent = document.createElement('div');
+            itemContent.className = 'col searchItemContent';
+            itemContent.textContent = 'نتیجه‌ای یافت نشد.';
+
+            container.appendChild(itemContent);
         }
     })
-    .catch(error => console.error('Error fetching data:', error));
+    .catch(error => {
+        const container = document.querySelector('.row.px-4.px-lg-5.searchItemBody');
+        const itemContent = document.createElement('div');
+        itemContent.className = 'col searchItemContent';
+        itemContent.textContent = error.toString();
+
+        container.appendChild(itemContent);
+
+        console.error('Error fetching data:', error)
+    });
